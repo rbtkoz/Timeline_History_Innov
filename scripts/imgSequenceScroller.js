20401348989d574b12efe2df9bdbd821850ac802
AK <kozovski.a@gmail.com>
2014-07-01 17:47:10 -0400

    scroll categories functioning

--- scripts/imgSequenceScroller.js
+++ scripts/imgSequenceScroller.js
@@ -14,12 +14,8 @@ $(canv)
 	.width($(window).width())
 	.height($(window).height());
 
-var currentLocation = 0;
 
-
-var currentMousePos = false;
-
-
+// var currentMousePos = false;
 // $(document).on("scrollLeft", function() {
 // 	console.log($("body").scrollLeft());
 // 	currentMousePos = scrollLeft();
@@ -47,43 +43,128 @@ var currentMousePos = false;
 // 	if(currentLocation > images.length) currentLocation = images.length;
 // 	setImage(currentLocation);
 // });
-var lastScroll = 0;
-window.scrollX =0;
+
+
+	var currentLocation = 0;
+	 var arr = Math.floor(images.length/4);
+	 // console.log(arr +"Lenght of array");
+	var lastScroll = 0;
+	// var loc =0;
+	window.scrollX = 0;
 //this resets to beginning 0 of experience
 var st = $('body').animate({scrollLeft: 0}, 1000);
 
 window.addEventListener('scroll', function(e) {
 
+
 	// e.preventDefault();
 	// console.log(window.scrollX);
+	// var st = $('body').animate({scrollLeft: incrementer}, 1000);
 
 	var st = $(this).scrollLeft();
- 	console.log(st +"st value")
+	
+
+ 	console.log(st +" st value")
 	if (st > lastScroll){
 
-		console.log("we are going left");
-		currentLocation += 1;
-		console.log("+1" +"currentLocation =" +currentLocation);
+		console.log("swiping left!");
+		currentLocation += 2;
+		console.log("+1" +"currentLocation = " +currentLocation);
+		// incrementer = incrementer +100;
+		// loc = currentLocation;
 
 
-	} else {
-		console.log("we are going right");
-		currentLocation -= 1;
+ 	} if (st < lastScroll ){
+		console.log("swiping right!");
+		currentLocation -= 2;
 		console.log("-1"+"currentLocation =" +currentLocation);
-	}
-	
-	if(currentLocation < 0) {
+		// incrementer = incrementer -100;
+		// loc = currentLocation;
+
+	} if (currentLocation < 0) {
+		console.log("im less than zero");
 		currentLocation = 0;
-	}	
-	if(currentLocation > images.length) {
+
+	
+	} if (currentLocation == parseInt(arr)){
+   	console.log("FIRST ");
+   	$("#one").toggleClass(' hidden');
+   	console.log("ON");
+  	
+  	} if ( currentLocation == parseInt(arr+20)){
+   	console.log("OFF");
+   	
+   	$("#one").toggleClass(" hidden");
+
+
+
+	} if (currentLocation == parseInt(arr*2)){
+   	console.log("SECOND ");
+   	$("#two").toggleClass("hidden");
+   	console.log("ON");
+  	
+  	} if ( currentLocation == parseInt((arr*2)+20)){
+   	console.log("OFF");
+   	
+   	$("#two").toggleClass(" hidden");
+
+
+
+	} if (currentLocation == parseInt(arr*3)){
+   	console.log("THIRD");
+   	$("#three").toggleClass("hidden");
+   	console.log("ON");
+  	
+  	} if ( currentLocation == parseInt((arr*3)+20)){
+   	console.log("OFF");
+   	
+   	$("#three").toggleClass("hidden");
+
+
+	} if (currentLocation == parseInt(images.length-20)){
+   	console.log("FOURTH");
+   	$("#four").toggleClass("hidden");
+   	console.log("ON");
+  	
+  	} if ( currentLocation == parseInt(images.length)){
+   	console.log("OFF");
+   	
+   	$("#four").toggleClass("hidden");
+
+	// } if (currentLocation < Math.floor(arr+30)){
+ //   	console.log("FIRST ");
+   	
+ //   	$("#one").removeClass('animated fadeOut').addClass("hidden");
+ //   	console.log("ADDDEDDEDEDEDEDDEDERFHGRTJHGSHTBSETHERBHWEH");
+
+
+  // } if (currentLocation > arr *2 && currentLocation < (arr *2)+20){
+  //  	console.log("SECOND ADDED");
+  //  	$("#one").toggleClass('hidden');
+  //  	$("#two").removeClass('hidden').addClass('animated fadeInDown');
+   
+  //  	} if (currentLocation > arr *3 && currentLocation < (arr *2)+20){
+  //  	console.log("THIRD");
+  //  	$("#two").addClass('hidden');
+  //  	$("#three").removeClass('hidden').addClass('animated fadeInDown');
+   
+  //  	} if (currentLocation > (images.length-20) && currentLocation < images.length){
+  //  	console.log("FOURTH");
+  //  	$("#three").addClass('hidden');
+  //  	$("#four").removeClass('hidden').addClass('animated fadeInDown');
+   
+  
+	}	if (currentLocation > images.length-1) {
+		console.log( 'i am too much');
 		currentLocation = images.length;
+		console.log("-1"+"currentLocation =" +currentLocation);
 		console.log(images.length +"how many images");
 	}
 			//some css animation on specific value -test
 			if (currentLocation >= 50) {
         $("p").removeClass("lead").addClass("load");
          $("i").addClass("faux");
-    } else {
+    } else  {
         $("p").addClass("lead").removeClass("load");
         $("i").removeClass("faux");
     }
@@ -95,6 +176,7 @@ window.addEventListener('scroll', function(e) {
 
 function setImage(newLocation) {
 	context.drawImage(images[newLocation], 0, 0, 1920, 1080);
+	console.log(images.length + "length of images");
 }
 
 
