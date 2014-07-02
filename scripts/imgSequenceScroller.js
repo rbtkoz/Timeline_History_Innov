9057371448cd12daf917078cd52fc165b0569129
Adam Perlis <adam.perlis@gmail.com>
2014-07-02 16:26:41 -0400

    attempting to get mousemove working

--- scripts/imgSequenceScroller.js
+++ scripts/imgSequenceScroller.js
@@ -46,22 +46,19 @@ $(canv)
 
 
 	var currentLocation = 0;
-	 var arr = Math.floor(images.length/4);
-	 // console.log(arr +"Lenght of array");
-	var lastScroll = 0;
-	// var loc =0;
-	window.scrollX = 0;
-//this resets to beginning 0 of experience
-var st = $('body').animate({scrollLeft: 0}, 1000);
 
-window.addEventListener('scroll', function(e) {
+$(document).mousemove(function( event ) {
 
+	var mouseX = event.pageX;
+
+ 	console.log(mouseX);
 
 	// e.preventDefault();
 	// console.log(window.scrollX);
 	// var st = $('body').animate({scrollLeft: incrementer}, 1000);
 
-	var st = $(this).scrollLeft();
+	var st = mouseX;
+	var lastScroll = 0;
 	
 
  	console.log(st +" st value")
@@ -70,63 +67,59 @@ window.addEventListener('scroll', function(e) {
 		console.log("swiping left!");
 		currentLocation += 2;
 		console.log("+1" +"currentLocation = " +currentLocation);
-		// incrementer = incrementer +100;
-		// loc = currentLocation;
 
 
  	} if (st < lastScroll ){
 		console.log("swiping right!");
 		currentLocation -= 2;
 		console.log("-1"+"currentLocation =" +currentLocation);
-		// incrementer = incrementer -100;
-		// loc = currentLocation;
 
 	} if (currentLocation < 0) {
 		console.log("im less than zero");
 		currentLocation = 0;
 
-	
-	} if (currentLocation == parseInt(arr)){
-   	console.log("FIRST ");
-   	$("#one").toggleClass("hidden");
-   	console.log("ON");
+}
+	// } if (currentLocation == parseInt(arr)){
+ //   	console.log("FIRST ");
+ //   	$("#one").toggleClass("hidden");
+ //   	console.log("ON");
   	
-  	} if ( currentLocation == parseInt(arr+20)){
-   	console.log("OFF");
+ //  	} if ( currentLocation == parseInt(arr+20)){
+ //   	console.log("OFF");
    	
-   	$("#one").toggleClass("hidden");
+ //   	$("#one").toggleClass("hidden");
 
 
 
-	} if (currentLocation == parseInt(arr*2)){
-   	console.log("SECOND ");
-   	$("#two").toggleClass("hidden");
-   	console.log("ON");
+	// } if (currentLocation == parseInt(arr*2)){
+ //   	console.log("SECOND ");
+ //   	$("#two").toggleClass("hidden");
+ //   	console.log("ON");
   	
-  	} if (currentLocation == parseInt((arr*2)+20)){
-   	console.log("OFF");
-   	$("#two").toggleClass("hidden");
+ //  	} if (currentLocation == parseInt((arr*2)+20)){
+ //   	console.log("OFF");
+ //   	$("#two").toggleClass("hidden");
 
 
 
-	} if (currentLocation == parseInt(arr*3)){
-   	console.log("THIRD");
-   	$("#three").toggleClass("hidden");
-   	console.log("ON");
+	// } if (currentLocation == parseInt(arr*3)){
+ //   	console.log("THIRD");
+ //   	$("#three").toggleClass("hidden");
+ //   	console.log("ON");
   	
-  } if (currentLocation == parseInt((arr*3)+20)){
-   	console.log("OFF");
-   	$("#three").toggleClass("hidden");
+ //  } if (currentLocation == parseInt((arr*3)+20)){
+ //   	console.log("OFF");
+ //   	$("#three").toggleClass("hidden");
 
-	} if (currentLocation == parseInt((arr*4)-30)){
-   	console.log("FOURTH");
-   	$("#four").toggleClass("hidden");
-   	console.log("ON");
+	// } if (currentLocation == parseInt((arr*4)-30)){
+ //   	console.log("FOURTH");
+ //   	$("#four").toggleClass("hidden");
+ //   	console.log("ON");
   	
-  } if (currentLocation == parseInt((arr*4)-10)){
-   	console.log("OFF");
+ //  } if (currentLocation == parseInt((arr*4)-10)){
+ //   	console.log("OFF");
    	
-   	$("#four").toggleClass("hidden");
+ //   	$("#four").toggleClass("hidden");
 
 	// } if (currentLocation < Math.floor(arr+30)){
  //   	console.log("FIRST ");
@@ -135,36 +128,36 @@ window.addEventListener('scroll', function(e) {
  //   	console.log("ADDDEDDEDEDEDEDDEDERFHGRTJHGSHTBSETHERBHWEH");
 
 
-  // } if (currentLocation > arr *2 && currentLocation < (arr *2)+20){
-  //  	console.log("SECOND ADDED");
-  //  	$("#one").toggleClass('hidden');
-  //  	$("#two").removeClass('hidden').addClass('animated fadeInDown');
+ //  } if (currentLocation > arr *2 && currentLocation < (arr *2)+20){
+ //   	console.log("SECOND ADDED");
+ //   	$("#one").toggleClass('hidden');
+ //   	$("#two").removeClass('hidden').addClass('animated fadeInDown');
    
-  //  	} if (currentLocation > arr *3 && currentLocation < (arr *2)+20){
-  //  	console.log("THIRD");
-  //  	$("#two").addClass('hidden');
-  //  	$("#three").removeClass('hidden').addClass('animated fadeInDown');
+ //   	} if (currentLocation > arr *3 && currentLocation < (arr *2)+20){
+ //   	console.log("THIRD");
+ //   	$("#two").addClass('hidden');
+ //   	$("#three").removeClass('hidden').addClass('animated fadeInDown');
    
-  //  	} if (currentLocation > (images.length-20) && currentLocation < images.length){
-  //  	console.log("FOURTH");
-  //  	$("#three").addClass('hidden');
-  //  	$("#four").removeClass('hidden').addClass('animated fadeInDown');
+ //   	} if (currentLocation > (images.length-20) && currentLocation < images.length){
+ //   	console.log("FOURTH");
+ //   	$("#three").addClass('hidden');
+ //   	$("#four").removeClass('hidden').addClass('animated fadeInDown');
    
   
-	}	if (currentLocation > images.length-1) {
-		console.log( 'i am too much');
-		currentLocation = images.length-1;
-		console.log("-1"+"currentLocation =" +currentLocation);
-		console.log(images.length +"how many images");
-	}
-			//some css animation on specific value -test
-			if (currentLocation >= 50) {
-        $("p").removeClass("lead").addClass("load");
-         $("i").addClass("faux");
-    } else  {
-        $("p").addClass("lead").removeClass("load");
-        $("i").removeClass("faux");
-    }
+	// }	if (currentLocation > images.length-1) {
+	// 	console.log( 'i am too much');
+	// 	currentLocation = images.length-1;
+	// 	console.log("-1"+"currentLocation =" +currentLocation);
+	// 	console.log(images.length +"how many images");
+	// }
+	// 		//some css animation on specific value -test
+	// 		if (currentLocation >= 50) {
+ //        $("p").removeClass("lead").addClass("load");
+ //         $("i").addClass("faux");
+ //    } else  {
+ //        $("p").addClass("lead").removeClass("load");
+ //        $("i").removeClass("faux");
+ //    }
     //updating value to latest 
 		lastScroll = st;
 		setImage(currentLocation);
